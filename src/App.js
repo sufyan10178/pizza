3f1dcf8832b815397d663abcfdc5c115276f0e9f
Suffyan Razzaq <nsuffyanrazzaq@CSNA.LAB>
2024-07-10 04:44:24 +0500

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Order from './components/Order';
+import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import image1 from './assets/slider5.png';
+import slideimage from './assets/slideImage.jpg';
+import image3 from './assets/slice.jpg';
+import hawaiianImage from './assets/hawaiian.png';
+import veggieImage from './assets/veggie.png';
+import bbqChickenImage from './assets/bbq_chicken.png';
 
 function App() {
+  const [carouselImages, setCarouselImages] = useState([image1, slideimage, image3]);
+
+  const loadPizzasFromStorage = () => {
+    const storedPizzas = localStorage.getItem('pizzas');
+    return storedPizzas ? JSON.parse(storedPizzas) : [
+      { id: 1, title: 'Margherita', description: 'Classic delight with 100% real mozzarella cheese', image: 'https://as2.ftcdn.net/v2/jpg/02/66/75/97/1000_F_266759706_9MfrbbBd2SleNS2YNWyg5qZulSspYYHX.jpg' },
+      { id: 2, title: 'Pepperoni', description: 'A classic American pizza with pepperoni', image: image3 },
+      { id: 3, title: 'BBQ Chicken', description: 'Grilled chicken with BBQ sauce', image: bbqChickenImage },
+      { id: 4, title: 'Hawaiian', description: 'Pizza with ham and pineapple', image: hawaiianImage },
+      { id: 5, title: 'Veggie', description: 'Loaded with a variety of vegetables', image: veggieImage },
+    ];
+  };
+
+  const [pizzas, setPizzas] = useState(loadPizzasFromStorage);
+
+  useEffect(() => {
+    localStorage.setItem('pizzas', JSON.stringify(pizzas));
+  }, [pizzas]);
+
+  const [open, setOpen] = useState(false);
+  const [currentPizza, setCurrentPizza] = useState({ id: '', title: '', description: '', image: '' });
+
+  const handleClickOpen = (pizza) => {
+    setCurrentPizza(pizza);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+    setCurrentPizza({ id: '', title: '', description: '', image: '' });
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setCurrentPizza({ ...currentPizza, [name]: value });
+  };
+
+  const handleSave = () => {
+    if (currentPizza.id) {
+      setPizzas(pizzas.map((pizza) => (pizza.id === currentPizza.id ? currentPizza : pizza)));
+    } else {
+      const newPizza = { ...currentPizza, id: pizzas.length + 1 };
+      setPizzas([...pizzas, newPizza]);
+    }
+    handleClose();
+  };
+
+  const handleDelete = (id) => {
+    setPizzas(pizzas.filter((pizza) => pizza.id !== id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <AppBar position="static" style={{ backgroundColor: '#ff5722' }}>
+          <Toolbar>
+            <Typography variant="h6" style={{ flexGrow: 1 }}>
+              Pizza
+            </Typography>
+            <Button color="inherit" href="/">Home</Button>
+            <Button color="inherit" href="/about">About</Button>
+            <Button color="inherit" href="/contact">Contact</Button>
+            <IconButton color="inherit" onClick={() => handleClickOpen({ id: '', title: '', description: '', image: '' })}>
+              <Add />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Routes>
+          <Route
+            path="/"
+            element={<Home
+              carouselImages={carouselImages}
+              pizzas={pizzas}
+              handleClickOpen={handleClickOpen}
+              handleDelete={handleDelete}
+              handleChange={handleChange}
+              handleSave={handleSave}
+              open={open}
+              handleClose={handleClose}
+              currentPizza={currentPizza} />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/order/:pizzaId" element={<Order pizzas={pizzas} />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/Home.js
@@ -0,0 +1,126 @@
+import React from 'react';
+import { Grid, Card, CardMedia, CardContent, Typography, IconButton, Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button, Container } from '@mui/material';
+import { Edit, Delete, Add } from '@mui/icons-material';
+import Slider from 'react-slick';
+import { Link } from 'react-router-dom';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+function Home({
+  carouselImages, pizzas, handleClickOpen, handleDelete, handleChange, handleSave, open, handleClose, currentPizza,
+}) {
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div>
+      <Slider {...sliderSettings}>
+        {carouselImages.map((image, index) => (
+          <div key={index}>
+            <img src={image} alt={`Pizza ${index + 1}`} style={{ width: '100%', height: '65vh', objectFit: 'cover' }} />
+          </div>
+        ))}
+      </Slider>
+      <Container>
+        <Typography variant="h4" style={{ marginTop: 30, marginBottom: 30, textAlign: 'center' }}>
+          Our Pizzas
+        </Typography>
+        <Grid container spacing={3}>
+          {pizzas.map((pizza) => (
+            <Grid item xs={12} sm={6} md={4} key={pizza.id}>
+              <Card style={{ maxWidth: 345, margin: 'auto' }}>
+                <CardMedia
+                  style={{ height: 140 }}
+                  image={pizza.image}
+                  title={pizza.title} />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {pizza.title}
+                  </Typography>
+                  <Typography variant="body2" color="textSecondary" component="p">
+                    {pizza.description}
+                  </Typography>
+                  <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginTop: 10 }}>
+                    <IconButton style={{color:'#f57953'}} onClick={() => handleClickOpen(pizza)}>
+                      <Edit />
+                    </IconButton>
+                    <IconButton style={{color:'#f57953'}} onClick={() => handleDelete(pizza.id)}>
+                      <Delete />
+                    </IconButton>
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      style={{ backgroundColor: '#ff5722' }}
+                      component={Link}
+                      to={`/order/${pizza.id}`}
+                    >
+                      Order Now
+                    </Button>
+                  </div>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{currentPizza.id ? 'Edit Pizza' : 'Add Pizza'}</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            name="title"
+            label="Title"
+            type="text"
+            fullWidth
+            value={currentPizza.title}
+            onChange={handleChange} />
+          <TextField
+            margin="dense"
+            name="description"
+            label="Description"
+            type="text"
+            fullWidth
+            value={currentPizza.description}
+            onChange={handleChange} />
+          <TextField
+            margin="dense"
+            name="image"
+            label="Image URL"
+            type="text"
+            fullWidth
+            value={currentPizza.image}
+            onChange={handleChange} />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleSave} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <footer style={{ backgroundColor: '#ff5722', color: '#fff', padding: '20px 0', marginTop: '25px' }}>
+        <Container>
+          <Typography variant="h6" align="center" gutterBottom>
+            Pizza
+          </Typography>
+          <Typography variant="subtitle1" align="center" color="inherit" component="p">
+            © 2024 Pizza Inc.
+          </Typography>
+          <Typography variant="body2" align="center" color="inherit" component="p">
+            Contact us: info@pizza.com | +92 3014220585
+          </Typography>
+        </Container>
+      </footer>
+    </div>
+  );
+}
+
+export default Home;
